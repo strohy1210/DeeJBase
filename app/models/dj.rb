836a2168dffa956e1d10acda71a3d73b65e08489
strohy120 <sarah.stroh@flatironschool.com>
2014-12-16 16:41:24 -0500

    working mehtods to get the informationnzz

--- app/models/dj.rb
+++ app/models/dj.rb
@@ -5,7 +5,7 @@ class Dj < ActiveRecord::Base
   has_many :venues, through: :events
   accepts_nested_attributes_for :venues
 
-  attr_accessor :message
+  attr_accessor :message, :demo_title
   
   def self.get_user_from_omniauth(auth_hash)
     self.find_with_omniauth(auth_hash) || self.create_with_omniauth(auth_hash)
@@ -25,34 +25,26 @@ class Dj < ActiveRecord::Base
     end
   end
 
-  def self.get_soundcloud_djs
-    
+  def self.get_soundcloud_djs(page)      
     client = Soundcloud.new(:client_id => 'ed094c22af47eec76cdc9d24005bcdec')
     page_size =200
-    # while i < 41
-    # i += 1   :offset => page_size * 2
-    client.get('/users', :q => 'New York', :limit=> page_size)
-
+    client.get('/users', :q => 'New York', :limit=> page_size, :offset => (page-1)*page_size)
   end
 
-  def self.create_sc_djs
-    self.get_soundcloud_djs.each do |dj|
-      
-      #only find ppl where "plan" != "Free", means their serious somewhat
+  def self.create_sc_djs(page)
+    self.get_soundcloud_djs(page).each do |dj|
       city = dj.city
       if city
-        if dj.description && city.downcase.include?("new york") || city.include?("NY") || city.downcase.include?("brooklyn") || city.downcase.include?("bronx") || city.downcase.include?("queens") || city.downcase.include?("staten")   
+        if dj.description && (city.downcase.include?("new york") || city.downcase.include?("ny") || city.downcase.include?("brooklyn") || city.downcase.include?("bronx") || city.downcase.include?("queens") || city.downcase.include?("staten"))
           email = dj.description.scan(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i).first
           if email && dj.plan != "Free"
             sdcl_followers = dj.followers_count
             image_url = dj.avatar_url
             name = dj.username
             sdcl_id = dj.id
-            bio = dj.description    
-          
+            bio = dj.description
             phone = dj.extract_phone_number(bio)
-          #webpage = dj.description.scan somethign
-            Dj.create(city: city, email: email, name: name, sdcl_followers: sdcl_followers, bio: bio, dj_status: true, sdcl_id: sdcl_id, phone: phone)
+            Dj.create(city: city, email: email, name: name, sdcl_followers: sdcl_followers, bio: bio, dj_status: true, sdcl_id: sdcl_id, phone: phone, image_url: image_url)
           end
         end
       end
@@ -62,18 +54,13 @@ class Dj < ActiveRecord::Base
   def self.get_demos
     client = Soundcloud.new(:client_id => 'ed094c22af47eec76cdc9d24005bcdec')
     Dj.where(demo: nil).each do |dj|
-      track = client.get('/tracks', :q => dj.name, :limit=> 1)
-      # track_url = track.track_url?
-      # embed_info = client.get('/oembed', :url => track_url)
-      # embed_info['html'] => the html for the embed player! i think
-      # dj.update(demo: track_url )
+      track = client.get('/tracks', :q => dj.name, :limit=> 1).first.permalink_url
+      embed_info = client.get('/oembed', :url => track)
+      dj.update(demo: embed_info['html'])
+      dj.demo_title = embed_info['title']
       # https://developers.soundcloud.com/docs/api/guide#playing
     end
   end
 
 end
-  
-  # dj = client.get('/users', :q => 'imaginarynumbers').first
-  # city = dj["city"]
-  # contact = dj["description"]
-  # followers =  dj["followers_count"]
+
