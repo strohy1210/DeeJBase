234e8318dc9365b97a34aa66e77d0c0e360b587f
strohy120 <sarah.stroh@flatironschool.com>
2014-12-24 21:46:09 -0500

    added modal for message form yay

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require_tree .
-  // $(function(){
-  //   $('#results').pageless({ totalPages: 10
-  //                        , url: '/'
-  //                        , loaderMsg: 'Loading more results'
-  //                        });
-
-  // })()
