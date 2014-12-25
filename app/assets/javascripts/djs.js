bd5783dfcea7818537b8ac41faca4d30297f1f29
strohy120 <sarah.stroh@flatironschool.com>
2014-12-24 22:07:22 -0500

    slightly improved update form style

--- app/assets/javascripts/djs.js
+++ app/assets/javascripts/djs.js
@@ -5,27 +5,29 @@ $(".update-form").hide();
 $('.edit-dj').click(function(){
   $(".update-form").toggle();
   $("#dj-profile").toggle();
+  $(this).toggle();
 })
 
 $('.cancel-edit').click(function(){
   $(".update-form").toggle();
   $("#dj-profile").toggle();
+  $('.edit-dj').toggle();
 })
 
-$(".message-form").hide();
-$('.hire-dj').click(function(){
-  $(".message-form").toggle();
-  $('.cancel-message').toggle()
-  // $(".profile-pic").toggle();
-  $(this).toggle();
-})
+// $(".message-form").hide();
+// $('.hire-dj').click(function(){
+//   $(".message-form").toggle();
+//   $('.cancel-message').toggle()
+//   // $(".profile-pic").toggle();
+//   $(this).toggle();
+// })
 
-$('.cancel-message').hide();
-$('.cancel-message').click(function(){
-  $(".message-form").toggle();
-    $(this).toggle();
-    $('.hire-dj').toggle();
-})
+// $('.cancel-message').hide();
+// $('.cancel-message').click(function(){
+//   $(".message-form").toggle();
+//     $(this).toggle();
+//     $('.hire-dj').toggle();
+// })
 
 $('.contact-form').hide();
 $('.send-contact').click(function(){
