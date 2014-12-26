b91266ca09a6a42c6aec11a894c448894d4b1d3b
strohy120 <sarah.stroh@flatironschool.com>
2014-12-26 10:47:55 -0500

    add dedupe method

--- app/models/track.rb
+++ app/models/track.rb
@@ -42,4 +42,16 @@ class Track < ActiveRecord::Base
     @genres
   end
 
+  def self.dedupe
+    # find all models and group them on keys which should be common
+    grouped = all.where.not(title: nil).group_by{|track| [track.title] }
+    grouped.values.each do |duplicates|
+      # the first one we want to keep right?
+      first_one = duplicates.shift # or pop for last one
+      # if there are any more left, they are duplicates
+      # so delete all of them
+      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+    end
+  end
+
 end
