d2748f84fb08dce084f750de343a7a8c8634cbb8
strohy120 <sarah.stroh@flatironschool.com>
2015-01-11 18:54:19 -0500

    fixed search bar

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,21 +2,24 @@ class SearchesController < ApplicationController
   skip_before_action :authorize
   def index
     if params[:name]
-      name = params[:name]
-      @djs = Dj.where('lower(name) LIKE ?', "%#{name.downcase}%").where(dj_status: true).paginate(page: params[:page], per_page: 6).order('created_at DESC')
+      name = params[:name].strip
+      @djs = Dj.is_dj.where('lower(name) LIKE ?', "%#{name.downcase}%").paginate(page: params[:page], per_page: 6).order('updated_at DESC') || Dj.is_dj.where('lower(name) LIKE ?', "%#{name.titleize.downcase}%").paginate(page: params[:page], per_page: 6).order('updated_at DESC')
       if @djs.size == 0
         flash[:warning] = 'Sorry, no one by that name... so here\'s everyone.'
         redirect_to root_path
+      elsif @djs.size == 1
+        @dj = @djs.first
+        redirect_to dj_path(@dj.slugify)
       else
         flash.now[:success] = @djs.size.to_s + ' result(s) for "' +name+'." Click for more info.'
         render 'welcome/index'
       end
     elsif params[:genre_id]
       @genre= Genre.find(params[:genre_id])
-      @djs = @genre.djs.where(dj_status: true, agent_status: false).paginate(page: params[:page], per_page: 6).order('created_at DESC')
+      @djs = @genre.djs.is_dj.paginate(page: params[:page], per_page: 6).order('created_at DESC')
       render 'welcome/index'
     else
-      @djs = Dj.where(dj_status: true, agent_status: false).paginate(page: params[:page], per_page: 6).order('created_at DESC')
+      @djs = Dj.where.is_dj.paginate(page: params[:page], per_page: 6).order('created_at DESC')
       render 'welcome/index'
     end
   end
