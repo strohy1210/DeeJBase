ad23d83aa906725a2835587231c578f5ffc60cd7
strohy120 <sarah.stroh@flatironschool.com>
2015-01-16 16:26:56 -0500

    works

--- app/models/yelp_data.rb
+++ app/models/yelp_data.rb
@@ -15,20 +15,24 @@ class YelpData
 
 
   def save_attributes(venue, yelp_venue)
-    if yelp_venue.location.cross_streets
-      cross_streets = yelp_venue.location.cross_streets
-    else
-      cross_streets = "x"
+    if yelp_venue.location
+      neighborhood = yelp_venue.location.neighborhoods.first
+      address = yelp_venue.location.address.first
+      latitude = yelp_venue.location.coordinate.latitude
+      longitude = yelp_venue.location.coordinate.longitude
+      if yelp_venue.location.cross_streets
+        cross_streets = yelp_venue.location.cross_streets
+      else
+        cross_streets = "x"
+      end
     end
-
-    neighborhood = yelp_venue.location.neighborhoods.first
+    
     phone = yelp_venue.phone
     yelp_rating = yelp_venue.rating
     image_url = yelp_venue.image_url
-    address = yelp_venue.location.address.first
+
     name= yelp_venue.name
-    latitude = yelp_venue.location.coordinate.latitude
-    longitude = yelp_venue.location.coordinate.longitude
+
     venue.update(cross_streets: cross_streets, neighborhood: neighborhood, phone: phone, name: name, yelp_rating: yelp_rating, image_url: image_url, address: address, latitude: latitude, longitude: longitude, yelp_id: yelp_venue.id)
   end
 end
