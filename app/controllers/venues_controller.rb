669f271675b0aa944593ff204b2f06ae811e83e1
strohy120 <sarah.stroh@flatironschool.com>
2015-01-21 15:58:18 -0500

    fix logout dissapear

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,20 +1,17 @@
 class VenuesController < ApplicationController
  skip_before_action :authorize
   
-  def index
-    
-    if params["category"] && params["category"] != "all"
+  def index  
+    if params["category"] && params["category"] != "all" && params[:neighborhood_id] != "all"
       @category = params["category"]
-      if params[:neighborhood_id] && params[:neighborhood_id] != "all"
-        @neighborhood = Neighborhood.find(params[:neighborhood_id])
-        @venues = Venue.where(neighborhood_id: @neighborhood.id, category: @category).paginate(page: params[:page], per_page: 6).order('updated_at DESC')
-        if @venues.blank?
-          flash[:warning] = "No venues matching that neighborhood and category, so these are the results for the neighborhood."
-          @venues = Venue.where(neighborhood_id: @neighborhood.id).paginate(page: params[:page], per_page: 6).order('updated_at DESC')
-        end
-      else
-        @venues = Venue.where(category: @category).paginate(page: params[:page], per_page: 6).order('updated_at DESC')
+      @neighborhood = Neighborhood.find(params[:neighborhood_id])
+      @venues = Venue.where(neighborhood_id: @neighborhood.id, category: @category).paginate(page: params[:page], per_page: 6).order('updated_at DESC')
+      if @venues.blank?
+        flash[:warning] = "No venues matching that neighborhood and category, so these are the results for the neighborhood."
+        @venues = Venue.where(neighborhood_id: @neighborhood.id).paginate(page: params[:page], per_page: 6).order('updated_at DESC')
       end
+    elsif params["category"] && params["category"] != "all" && params[:neighborhood_id] == "all"
+      @venues = Venue.where(category: @category).paginate(page: params[:page], per_page: 6).order('updated_at DESC')
     elsif params["category"] == "all" && params[:neighborhood_id] && params[:neighborhood_id] != "all"
       @neighborhood = Neighborhood.find(params[:neighborhood_id])
       @venues = Venue.where(neighborhood_id: @neighborhood.id).paginate(page: params[:page], per_page: 6).order('updated_at DESC')
