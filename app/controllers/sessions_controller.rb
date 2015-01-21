3477297b1ed52a6c97edbc39cf3fd2eab3309700
strohy120 <sarah.stroh@flatironschool.com>
2015-01-21 18:24:42 -0500

    add venue pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to dj_form_path
     elsif @user.dj_status
       @dj = current_dj
-      redirect_to dj_path(@dj.slugify)
+      redirect_to :back
     else
       redirect_to :back
     end
