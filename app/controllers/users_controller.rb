401f995414868cba3eb8213a3abaacb06384f590
strohy120 <sarah.stroh@flatironschool.com>
2015-02-10 15:09:07 -0500

    slightly better show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @comments = @user.comments.select {|c| c.valid? && c.is_valid?}
+    if @user.comments
+      @comments = @user.comments.select {|c| c.valid? && c.is_valid?}
+    end
   end
 end
