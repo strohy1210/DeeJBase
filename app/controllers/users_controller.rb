82c02b79ded23f9ec404312f7f34b421594d1790
strohy120 <sarah.stroh@flatironschool.com>
2015-02-10 14:17:15 -0500

    display only valid comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @comments = @user.comments
+    @comments = @user.comments.select {|c| c.valid? && c.is_valid?}
   end
 end
