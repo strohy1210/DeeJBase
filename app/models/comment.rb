f2f46cce6a0b9f21913d7b6b1a7d1f3123b3b9ea
strohy120 <sarah.stroh@flatironschool.com>
2015-04-14 11:09:31 -0400

    fix callback for rated at

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,7 +8,6 @@ class Comment < ActiveRecord::Base
   validates :rating_id, presence: :true, on: :update
   before_update :check_rating
   has_many :replies
-  after_update :update_rated_at
 
   def check_rating
     rating.destroy unless rating.valid?
@@ -18,12 +17,6 @@ class Comment < ActiveRecord::Base
     body.length >= 40 if body
   end
 
-  def update_rated_at
-    if self.dj && body.size > 40 && rating && rating.score > 0
-      dj.update(rated_at: self.updated_at)
-    elsif self.venue && body.size > 40 && rating && rating.score > 0
-      venue.update(rated_at: self.updated_at)
-    end
-  end
+
 end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,8 +5,17 @@ class Event < ActiveRecord::Base
   has_many :comments, through: :ratings
   has_many :users, through: :ratings
   after_update :check_for_users
+  after_create :update_venue_dj_rated_at
 
   def check_for_users
     destroy if users.blank?
+  end
+
+  def update_venue_dj_rated_at
+    if self.dj
+      self.dj.update(rated_at: self.created_at)
+    elsif self.venue
+      self.venue.update(rated_at: self.created_at)
+    end
   end
 end
