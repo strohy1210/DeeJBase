68780356a6709e309061bb62cddb714be3954397
strohy120 <sarah.stroh@flatironschool.com>
2015-06-16 18:19:10 -0400

    slightly more legible event listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615182703) do
+ActiveRecord::Schema.define(version: 20150616214333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 20150615182703) do
     t.text     "description"
     t.string   "image_url"
     t.datetime "rated_at"
+    t.string   "url"
   end
 
   create_table "ratings", force: true do |t|
