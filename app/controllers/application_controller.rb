cae7b8af37ace150ff191a553d470a46012b3dbc
strohy120 <sarah.stroh@flatironschool.com>
2015-07-08 09:28:57 -0400

    add festival to navabar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -90,13 +90,13 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def mobile_device?
-      # if session[:mobile_param]
-      #   session[:mobile_param] == "1"
-      # else
-        request.user_agent =~ /Mobile|webOS/
-      # end
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
     end
+  end
 
     def prepare_for_mobile
       session[:mobile_param] = params[:mobile] if params[:mobile]
