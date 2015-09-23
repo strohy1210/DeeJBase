1f097b1cf8411be8a8f2dcda21bfcfe6e63beb55
strohy120 <sarah.stroh@flatironschool.com>
2015-09-22 20:33:13 -0400

    slightly updated user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     if @user.ratings
-      @ratings = @user.ratings.valid_only
+      @ratings = @user.ratings.where.not(event_id: nil).select {|r| r.comment && r.comment.is_valid? && r.comment.valid? && r.valid? && r.score != 0}
     end
   end
 end
